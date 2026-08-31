10eec2622341754b8a19aa8065597b90322e84e6
Shawn <ctpdrqm@foxmail.com>
2026-08-31 14:07:12 +0800

    Initial Commit

---
+++ NoTun4Antigravity/NoTun4AntigravityApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoTun4AntigravityApp.swift
+//  NoTun4Antigravity
+//
+//  Created by cheng on 2026/8/31.
+//
+
+import SwiftUI
+
+@main
+struct NoTun4AntigravityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoTun4AntigravityTests/NoTun4AntigravityTests.swift
@@ -0,0 +1,19 @@
+//
+//  NoTun4AntigravityTests.swift
+//  NoTun4AntigravityTests
+//
+//  Created by cheng on 2026/8/31.
+//
+
+import Testing
+@testable import NoTun4Antigravity
+
+struct NoTun4AntigravityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
